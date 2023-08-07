a08c10e61515d2acfe756a18619a43eba61eb218
Sayed22y <sayedshaltoot727@gmail.com>
2023-08-07 15:10:15 +0300

    capstone2

--- capstone 2222.py
+++ capstone 2222.py
@@ -8,17 +8,25 @@ import plotly as px
 
 
 
-df = pd.read_csv("C:/Users/Elsayed Hassan/.spyder-py3/autosave/International_Report_Departures.csv").head(5000)
+data = pd.read_csv("C:/Users/Elsayed Hassan/.spyder-py3/autosave/International_Report_Departures.csv")
+
+#x = data.iloc[:,:-1]
+#y = data.iloc[:,-1]
+
+df = pd.DataFrame(data)
+df = df.drop("data_dte", axis=1)
 
 
-column_types = {col : "numerical" if df[col].dtype in ["int64","float64"] else "categorical" for col in df.columns}
+
+
+column_types = {col : "numerical" if df[col].dtype in ["int64","float64"]  else "categorical" for col in df.columns}
 
 
 df.fillna(method = "ffill", inplace=True)
 
 
 
-newdf = pd.get_dummies(df, columns=[col for col , col_type in column_types.items() if col_type == "categorical"])
+df = pd.get_dummies(df, columns=[col for col , col_type in column_types.items() if col_type == "categorical"])
 
 data_numerical = df[[col for col, col_type in column_types.items() if col_type == 'numerical']]
 data_normalized = (data_numerical - data_numerical.min()) / (data_numerical.max() - data_numerical.min())
@@ -36,7 +44,7 @@ for col, col_type in column_types.items():
         
         plt.figure()
         sns.boxplot(df[col])
-        plt.title("fbox plot for {col}")
+        plt.title(f"box plot for {col}")
         plt.show()
         
         
